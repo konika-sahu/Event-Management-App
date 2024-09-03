391b82674854c84273c0e5d258683b8243fae362
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-09-03 23:49:18 +0530

    modify Event model.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -18,7 +18,7 @@ class Event extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function attendee() : HasMany
+    public function attendees() : HasMany
     {
         return $this->hasMany(Attendee::class);
     }
