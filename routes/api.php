d48718528b8d8968223b9fdf68dede9cbdade5ec
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-09-03 23:53:14 +0530

    modify api-php file, update the route definition by calling except() method, specifying we don't need update action in the AttendeeController.

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,5 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
-    -> scoped( ['attendee' => 'event'] );
+    -> scoped() -> except( [ 'update' ] );
 
