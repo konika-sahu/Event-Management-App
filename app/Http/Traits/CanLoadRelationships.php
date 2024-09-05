d83e9f4cd7e4cb5d1aa5eefc46187107150f99f0
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-09-06 00:07:48 +0530

    modify CanLoadRelationships trait, to add HasMany class to load the additional relationship with the Attendees and User.

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -4,11 +4,12 @@ namespace App\Http\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 trait CanLoadRelationships
 {
-    public function loadRelationships( Model|EloquentBuilder|QueryBuilder $for, ?array $relations = null) : Model|EloquentBuilder|QueryBuilder
+    public function loadRelationships( Model|EloquentBuilder|QueryBuilder|HasMany $for, ?array $relations = null) : Model|EloquentBuilder|QueryBuilder|HasMany
     {
         $relations = $relations ?? $this->relations ?? [];
 
@@ -28,7 +29,7 @@ trait CanLoadRelationships
         if(!$include){
             return false;
         }
-        
+
         $relations = array_map('trim', explode(',', $include));
 
         return in_array($relation, $relations);
