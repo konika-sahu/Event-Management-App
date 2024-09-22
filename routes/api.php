eebdc7bc1b30a8f3cac032191a39233f7a37eea2
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-09-22 17:35:52 +0530

    modify routes api, add authentication for destroy route.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Sanctum;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,6 +21,11 @@ Route::apiResource('events', EventController::class)->only(['index', 'show']);
 // Protected routes (middleware applied)
 Route::apiResource('events', EventController::class)->except(['index', 'show'])->middleware('auth:sanctum');
 
+// Public routes (actions that don't require authentication)
 Route::apiResource('events.attendees', AttendeeController::class)
-    -> scoped() -> except( [ 'update' ] );
+    -> scoped() -> except( [ 'update', 'destroy'] ); 
 
+// Authenticated route for 'destroy' action
+Route::middleware('auth:sanctum')->group(function () {
+    Route::delete('events/{event}/attendees/{attendee}', [AttendeeController::class, 'destroy']);
+});
